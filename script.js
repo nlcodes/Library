01ff0bbdb38cd949542122c0847842cd5ca97e7f
Nathan <pythoniszen@gmail.com>
2023-12-18 02:12:13 -0800

    12/18/23 Add foundations for user interface as well as update displayBooks function

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const myLibrary = [{name: "Moby Dick", author: "Herman Melville"}, {name: "Breath", author: "James Nestor"},{name: "Grokking Algorithms", author: "Aditya Bhargava"}];
+const myLibrary = [{title: "Moby Dick", author: "Herman Melville", pages: 625, read: false}, {title: "Breath", author: "James Nestor", pages: 304, read: false},{title: "Moby Dick", author: "Herman Melville", pages: 625, read: false}, {title: "Breath", author: "James Nestor", pages: 304, read: false},{title: "Moby Dick", author: "Herman Melville", pages: 625, read: false}, {title: "Breath", author: "James Nestor", pages: 304, read: false}, {title: "Breath", author: "James Nestor", pages: 304, read: false}, {title: "Breath", author: "James Nestor", pages: 304, read: false}, {title: "Breath", author: "James Nestor", pages: 304, read: false}, {title: "Breath", author: "James Nestor", pages: 304, read: false}, {title: "Breath", author: "James Nestor", pages: 304, read: false},];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
@@ -14,12 +15,29 @@ function displayBooks() {
   for (const book of myLibrary) {
     const books = document.getElementById("books");
     const bookCard = document.createElement("div");
-    bookCard.classList.add("bookCard");
+    bookCard.classList.add("book-card");
+    const list = document.createElement("ul")
+    const title = document.createElement("li");
+    const author = document.createElement("li");
+    const pages = document.createElement("li");
+    const readBoolButton = document.createElement("button");
+    const removeButton = document.createElement("button");
+    readBoolButton.classList.add("read-bool-button")
+    removeButton.classList.add("remove-button")
 
-    const listItem = document.createElement("p");
-    listItem.textContent = "Title: " + book.name;
+    readBoolButton.textContent = "Read"
+    removeButton.textContent = "remove"
+
+    title.textContent = "Title: " + book.title;
+    author.textContent = "Author: " + book.author;
+    pages.textContent = "Pages: " + book.pages.toString();
+    
     books.appendChild(bookCard);
-    bookCard.appendChild(listItem);
+    bookCard.appendChild(list);
+    list.appendChild(title);
+    list.appendChild(author);
+    list.appendChild(pages);
+    bookCard.appendChild(readBoolButton);
   }
 }
 
