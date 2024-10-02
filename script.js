882a6238bd586ecc1118a7fdb89a96fab97bb283
Nathan Lusk <nate@Nathans-MBP.home>
2024-10-01 23:13:39 -0700

    10/01/24 Create function for adding new books to the library handling form submission with modals/dialogs | Implement function to close modal

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 const myLibrary = [{ title: "Moby Dick", author: "Herman Melville", pages: 625, read: false }];
 
+// Store the form in a variable to collect data with submit event
+const form = document.getElementById("book-data");
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -16,6 +19,9 @@ function removeBook(index) {
   myLibrary.splice(index, 1); // Remove the book from the array
   displayBooks(); // Redisplay the updated library
 }
+
+
+
 
 function displayBooks() {
   const books = document.getElementById("books");
@@ -50,10 +56,19 @@ function displayBooks() {
 
     const modal = document.querySelector("#modal");
     const openModal = document.querySelector("#add-book-btn");
+    const closeModalBtns = document.querySelectorAll(".close-modal-button");
 
     openModal.addEventListener("click", () => {
       modal.show();
     })
+    
+    closeModalBtns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        modal.close();
+
+      });      
+    });
+    
 
     books.appendChild(bookCard);
     bookCard.appendChild(list);
@@ -64,8 +79,27 @@ function displayBooks() {
     list.appendChild(removeLi);
     checkLi.appendChild(readBoolCheck);
     removeLi.appendChild(removeButton);
+      
   });
 }
 
 // Initially display the books
 displayBooks();
+
+// Listen for new book submissions and add to create new objects with constructor and add to myLibrary array
+form.addEventListener("submit", function(event) {
+
+  event.preventDefault();//prevent default form submission to server
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+
+  const newBook = new Book(title, author, pages);
+
+  myLibrary.push(newBook);
+
+  displayBooks();
+
+  form.reset();
+})
